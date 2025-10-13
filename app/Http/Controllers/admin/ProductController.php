59bcfdc972b7d945b27d109e835ddff743ef79cb
jitendra <jitendrak2050@gmail.com>
2025-10-13 08:10:39 +0530

    update

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Products;
+use App\Models\products;
 use App\Models\User;
 use Illuminate\Http\Request;
 
